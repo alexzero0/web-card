4a21e45f1cb4d92261d4f2ffd81bcc0cd67fb433
Alex Zero <demidov1245ltl@gmail.com>
2023-01-30 23:08:57 +0300

    Add loader and command export

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
   reactStrictMode: true,
   basePath: basePath,
   assetPrefix: `${basePath}/`,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
 }
 
 module.exports = nextConfig
