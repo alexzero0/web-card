1c8732d782d32c10eb49b25cac02a77f94c8bb42
Alex Zero <demidov1245ltl@gmail.com>
2023-02-07 14:59:20 +0300

    Rename file

--- src/componets/common/icons/index.js
+++ src/componets/common/icons/index.js
@@ -1,2 +1,2 @@
-export { default as HamburgerIcon } from './hamburger/Hamburger';
-export { default as InstagramIcon } from './instagram/Instagram';
+export { default as HamburgerIcon } from './hamburger';
+export { default as InstagramIcon } from './instagram';
